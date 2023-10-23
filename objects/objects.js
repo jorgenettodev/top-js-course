707f4d5169f321723bbc3794678302c4a977c1fe
Jorge Netto <jorgenetto96@gmail.com>
2023-10-23 04:30:52 -0300

    Object literals.

---
+++ objects/objects.js
@@ -0,0 +1,15 @@
+// Object literal
+
+const myObject = {
+    property: 'Value!',
+    otherProperty: 77,
+    "obnoxious property": function() {
+        // do stuff!
+    }
+}
+
+// getting information out of an object
+
+// dot notation
+myObject.property // 'Value!'
+myObject.["obnoxious property"] // [Function]
