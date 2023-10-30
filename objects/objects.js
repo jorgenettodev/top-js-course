f79596c18ebd2f672aa44be98f2130e3b9260fcf
Jorge Netto <jorgenetto96@gmail.com>
2023-10-30 11:47:42 -0300

    Prototypal Inheritance.

--- objects/objects.js
+++ objects/objects.js
@@ -1,32 +1,3 @@
-// Object literal
-
-const myObject = {
-    property: 'Value!',
-    otherProperty: 77,
-    "obnoxious property": function() {
-        // do stuff!
-    }
-}
-
-const person = {
-    name: "John",
-    age: 30,
-    isStudent: true,
-    "string property": "This is indeed a string property!",
-    greet: function() {
-        return `Hi, my name is ${this.name}.`;
-    },
-
-}
-
-// getting information out of an object
-
-// dot notation
-myObject.property // 'Value!'
-myObject["obnoxious property"] // [Function]
-
-// Object constructors
-
 function Person(name, age, isStudent) {
     this.name = name;
     this.age = age;
@@ -37,29 +8,48 @@ function Person(name, age, isStudent) {
     }
 }
 
+function Person(name) {
+    this.name = name;
+}
+
+Person.prototype.sayName = function() {
+    console.log(`Hello, I'm ${this.name}!`)
+}
+
 function Player(name, marker) {
     this.name = name;
     this.marker = marker;
-    this.sayName = function() {
-        return `My name is ${this.name}.`;
-    };
 }
 
-const player1 = new Player("Netto", "X");
+Player.prototype.getMarker = function() {
+    console.log(`My marker is ${this.marker}`)
+}
 
-// Exercise - Write a constructor for making "Book" objects. It should have the book's title, author, the number of pages, and whether or not it was read yet.
+// Make 'Player' objects inherit from `Person`
+Object.setPrototypeOf(Player.prototype, Person.prototype);
 
-function Book(title, author, numPages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages,
-    this.isRead = isRead;
+const player1 = new Player("steve", "X");
+const player2 = new Player("also steve", "O");
 
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${numPages} pages, ${this.isRead}`;
+
+
+
+
+function testExercise() {
+    // Exercise - Write a constructor for making "Book" objects. It should have the book's title, author, the number of pages, and whether or not it was read yet.
+
+    function Book(title, author, numPages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages,
+        this.isRead = isRead;
+
+        this.info = function() {
+            return `${this.title} by ${this.author}, ${numPages} pages, ${this.isRead}`;
+        }
     }
+
+    const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 295, "not read yet");
+
+    console.log(theHobbit.info());
 }
-
-const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 295, "not read yet");
-
-console.log(theHobbit.info());
