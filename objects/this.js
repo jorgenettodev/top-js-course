f690c666f34d827e56f00fd78608d3fca5672458
Jorge Netto <jorgenetto96@gmail.com>
2023-12-14 05:45:07 -0300

    more testing.

--- objects/this.js
+++ objects/this.js
@@ -12,3 +12,66 @@ function testArrowFunction() {
     cat.logInfo();
 }
 
+// create a Exercise function constructor with name and mastered (boolean);
+function testExercise() {
+    function Exercise(name, learned) {
+        this.name = name;
+        this.learned = learned;
+    
+        this.learn = () => {
+            if (!this.learned) {
+                console.log(`Now I can do ${this.name} skill!`);
+                this.learned = true;
+            }
+        }
+    }
+    const pullup = new Exercise('Pull-up', false);
+
+    pullup.learn();
+}
+class Skill {
+    constructor(name, learned, reps) {
+        this.name = name;
+        this.learned = learned;
+        this.reps = reps;
+    }
+
+    learn() {
+        this.learned = true;
+        console.log(`I can do at least one rep of ${this.name}`);
+    }
+}
+
+// Constructor invocation
+const hspu = new Skill('HSPU', false);
+
+hspu.learn(); 
+console.log(hspu.name); // write the output
+
+function foo() {
+    this.property = 'Default Value';
+}
+
+const fooInstance = new foo();
+console.log(fooInstance.property); // Default value;
+
+class Bar {
+    constructor() {
+        this.property = 'Default Value';
+    };
+}
+
+const barInstance = new Bar();
+console.log(barInstance.property);
+
+function Vehicle(type, wheelsCount) {
+    this.type = type;
+    this.wheelsCount = wheelsCount;
+    return this;
+}
+
+// Function invocation
+const car = Vehicle('Car', 4);
+
+
+console.log('testing code runner');
